d9e362935b4f45e580aa925a1ab0e71fa78a6966
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2010-04-26 15:39:01 +0200

    Fixed the clojure and clojure-swank dependencies
    Depends now on clojure 1.1.0 and lein-swank 1.1.0.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject hiredman.repl "0.0.1-SNAPSHOT"
   :description ""
   :url "http://github.com/hiredman/Repl"
-  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]]
-  :dev-dependencies [[org.clojure/swank-clojure "1.0"]])
+  :dependencies [[org.clojure/clojure "1.1.0"]]
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
